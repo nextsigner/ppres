5a4f71d72e5d16be5602b686fbfbaa517c6efdd3
nextsigner <nextsigner@gmail.com>
2020-07-16 02:18:25 -0300

    se crean funciones para registrar productos

--- index.js
+++ index.js
@@ -17,6 +17,7 @@ var path = require('path');
 var express = require('express');
 var bodyParser = require('body-parser');
 var app = express();
+app.use(express.static(__dirname));
 app.set('port', process.env.PORT || puertoApp);
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
--- prods.js
+++ prods.js
@@ -1,20 +1,80 @@
-module.exports=function(app){
+﻿module.exports=function(app){
 
     const Producto = require('./models/Productos')
 
     nuevoProducto = function(req, res){
-        let producto = new Producto()
-        producto.nombre = req.query.nombre
-        producto.precio=req.query.precio
-        producto.fechaRegistro = new Date(Date.now())
-        console.log('Creando un nuevo producto nombre: '+producto.nombre+' precio: '+producto.precio)
-        producto.save(function(err, userRegistered){
-            if(err){
-                res.status(500).send(`Error when user register: ${err}`)
-                return
-            }
-            res.status(200).send({producto: userRegistered})
-        })
+        console.log('Insertando producto con nombre '+req.query.nombre)
+        Producto.find({
+                         //date: {$gt: h }
+                         //date: {$gte: "2019-06-12T00:00:00+01:00", $lte: "2019-12-12T23:00:00+01:00" }
+                         //date: {$gte: h, $lte: hf }
+                         nombre:req.query.nombre // Search Filters
+                     },
+                     ['nombre'], // Columns to Return
+                     {
+                         skip:0, // Starting Row
+                         limit:1, // Ending Row
+                         sort:{
+                             nombre: 1 //Sort by Date Added DESC
+                         }
+                     },
+                     function(err,mensajes){
+                         if(err) res.status(500).send({mensaje: `Error al buscar mensajes: ${err}`})
+                         if(mensajes.length===0){
+                             console.log('Registrando el dato del producto '+req.query.nombre);
+                             //res.redirect('/res-add-producto.html?res=no'+mensajes.length)
+                             //Registra el producto porque no existe ninguno con ese nombre
+                             let producto = new Producto()
+                             producto.nombre = req.query.nombre
+                             producto.precio=req.query.precio
+                             producto.fechaRegistro = new Date(Date.now())
+                             console.log('Creando un nuevo producto nombre: '+producto.nombre+' precio: '+producto.precio)
+                             producto.save(function(err, userRegistered){
+                                 if(err){
+                                     res.status(500).send(`Error when user register: ${err}`)
+                                     return
+                                 }
+                                 res.redirect('/res-add-producto.html?res=El+producto+se+ha+agregado+correctamente&pid='+userRegistered._id)
+                                 //res.status(200).send({producto: userRegistered})
+                             })
+                             return
+                         }else{
+                             console.log('Se intenta repetir el registro del producto '+req.query.nombre);
+                             let msg='No+se+ha+registrado+el+productoYa+existe+un+producto+con+el+nombre+'+(''+req.query.nombre).replace(/ /g, '%20')
+                             res.redirect('/res-add-producto.html?res='+msg)
+                         }
+                     })
+        //        let nom= req.query.nombre
+//        Producto.find({nombre : "alarma 34"}, function(err, producto){
+//            if(err){
+//                res.status(500).send({prod: `Error al buscar producto: ${err}`})
+//                return
+//            }
+//            console.log('prod '+producto._id)
+//            if(!producto){
+//                console.log('No había producto con nombre '+req.query.nombre)
+//                //Registra el producto porque no existe ninguno con ese nombre
+//                let producto = new Producto()
+//                producto.nombre = req.query.nombre
+//                producto.precio=req.query.precio
+//                producto.fechaRegistro = new Date(Date.now())
+//                console.log('Creando un nuevo producto nombre: '+producto.nombre+' precio: '+producto.precio)
+//                producto.save(function(err, userRegistered){
+//                    if(err){
+//                        res.status(500).send(`Error when user register: ${err}`)
+//                        return
+//                    }
+//                    res.redirect('/res-add-producto.html?res=El+producto+se+ha+agregado+correctamente&pid='+userRegistered._id)
+//                    //res.status(200).send({producto: userRegistered})
+//                })
+//                return
+//            }
+//            //res.redirect('/res-add-producto.html?res=321')
+//            let msg='No+se+ha+registrado+el+productoYa+existe+un+producto+con+el+nombre+'+(''+nom).replace(/ /g, '%20')
+//            res.redirect('/res-add-producto.html?res='+msg)
+//            //res.status(200).send({'producto': producto})
+//        })
+
     }
 
     setChatUser = function(req, res){
