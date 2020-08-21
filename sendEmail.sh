48134b4b427bca1d00b1dffc2930f14ad729e649
nextsigner <nextsigner@gmail.com>
2020-08-21 18:24:44 -0300

    se agrega funcion email 4

--- sendEmail.sh
+++ sendEmail.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-echo "$1" | mail -a "From: Ppres <pizarromario@gmail.com>" -s "$2" $3
+echo "$1" | mail -a "Content-Type: text/html; charset=UTF-8" -a "From: Ppres <pizarromario@gmail.com>" -s "$2" $3
