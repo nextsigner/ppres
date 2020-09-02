12559649785e98e598a580b7070625d25a933908
nextsigner <nextsigner@gmail.com>
2020-09-02 12:30:33 -0300

    se sube ppres.desktop

--- install.sh
+++ install.sh
@@ -6,9 +6,7 @@ sudo chmod 755 sendEmail.sh
 
 
 #Instalando entorno de escritorio y aplicaciones necesarias para zodiacserver
-#sudo apt-get install -y nano xfce4 xfce4-goodies gnome-icon-theme tightvncserver nodejs qtquick1-5-dev qtscript5-dev
-
-sudo apt-get install -y nano
+sudo apt-get install -y nano xfce4 xfce4-goodies gnome-icon-theme tightvncserver
 
 sudo apt-get install postfix mailutils libsasl2-2 libsasl2-modules
 sudo dpkg-reconfigure postfix
@@ -19,24 +17,24 @@ sudo apt-get install -y npm
 
 npm install
 
-npm install forever -g
-npm install -g forever-service
+#npm install forever -g
+#npm install -g forever-service
 #sudo forever-service install ppres --script /root/ppres/index.js
 #sudo service ppres start
 
-#echo "install.sh copiando archivo /etc/init.d/vncserver..."
-#sudo cp xstartup ~/.vnc/xstartup
-#sudo chmod +x ~/.vnc/xstartup
+echo "install.sh copiando archivo /etc/init.d/vncserver..."
+sudo cp xstartup ~/.vnc/xstartup
+sudo chmod +x ~/.vnc/xstartup
 
 #echo "install.sh copiando archivo /etc/init.d/vncserver..."
-#sudo cp vncserver /etc/init.d/vncserver
-#sudo chmod +x /etc/init.d/vncserver
-#sudo update-rc.d vncserver defaults
+sudo cp vncserver /etc/init.d/vncserver
+sudo chmod +x /etc/init.d/vncserver
+sudo update-rc.d vncserver defaults
 
 #echo "install.sh copiando archivo ~/.config/autostart/mercurio.desktop..."
-#sudo mkdir ~/.config/autostart
-#sudo cp mercurio.desktop ~/.config/autostart/mercurio.desktop
-#sudo chmod +x ~/.config/autostart/mercurio.desktop
+sudo mkdir ~/.config/autostart
+sudo cp ppres.desktop ~/.config/autostart/ppres.desktop
+sudo chmod +x ~/.config/autostart/ppres.desktop
 
 #echo "install.sh copiando archivo /root/mercurio-server/mercurio-server-nodejs.sh..."
 #sudo mercurio-server-nodejs.sh /root/mercurio-server/mercurio-server-nodejs.sh
